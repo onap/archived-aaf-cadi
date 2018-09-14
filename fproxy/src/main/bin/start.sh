e26c7351cf20f6f5f29a4450c7b5c2e0a44e3ce5
Ravi Geda <gravik@amdocs.com>
2018-09-14 10:14:23 +0100

    Add forward proxy code
    Add forward proxy as a submodule to cadi.
    Though fproxy is a submodule it does not inherit from cadi's pom.
    
    Change-Id: Ie9083c8a54e88647396e59d4129c04312dfe450a
    Issue-ID: AAI-1603
    Signed-off-by: Ravi Geda <gravik@amdocs.com>

---
+++ fproxy/src/main/bin/start.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# ============LICENSE_START=======================================================
+# org.onap.aai
+# ================================================================================
+# Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+# Copyright © 2017-2018 European Software Marketing Ltd.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+
+BASEDIR="/opt/app/fproxy"
+
+if [ -z "${KEY_STORE_PASSWORD}" ]; then
+	echo "KEY_STORE_PASSWORD must be set in order to start up process"
+	exit 1
+fi
+
+PROPS="-DKEY_STORE_PASSWORD=${KEY_STORE_PASSWORD}"
+JVM_MAX_HEAP=${MAX_HEAP:-1024}
+
+exec java -Xmx${JVM_MAX_HEAP}m ${PROPS} -jar ${BASEDIR}/fproxy-exec.jar
