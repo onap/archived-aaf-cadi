40bfef83ca873e62b7ef6a8ea4576af9b057e60b
Lee, Tian (tl5884) <TianL@amdocs.com>
2018-09-20 12:27:40 +0100

    Initial drop of rProxy code
    The Reverse Proxy sidecar is used to separate the responsibility of
    authentication and authorization away from the primary microservice. In
    conjunction with the Forward Proxy sidecar, it is responsible for
    controlling access to the REST URL endpoints exposed by the primary
    microservice, and propogating security credentials to downstream
    microservices.
    
    Change-Id: I5d80429e5422d7b3937cde73ac10c2ec00d264e8
    Issue-ID: AAI-1604
    Signed-off-by: Lee, Tian (tl5884) <TianL@amdocs.com>

---
+++ sidecar/rproxy/src/main/java/org/onap/aaf/rproxy/config/ForwardProxyProperties.java
@@ -0,0 +1,67 @@
+/**
+ * ﻿============LICENSE_START=======================================================
+ * org.onap.aaf
+ * ================================================================================
+ * Copyright © 2018 European Software Marketing Ltd.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.aaf.rproxy.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("file:${CONFIG_HOME}/forward-proxy.properties")
+@ConfigurationProperties(prefix = "forward-proxy")
+public class ForwardProxyProperties {
+
+    private String protocol;
+    private String host;
+    private int port;
+    private String cacheurl;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public String getCacheurl() {
+        return cacheurl;
+    }
+
+    public void setCacheurl(String cacheurl) {
+        this.cacheurl = cacheurl;
+    }
+}
